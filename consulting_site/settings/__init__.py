497561bd973845102685910a5227d80b206c3646
Ubuntu <ubuntu@vps-b102f570.vps.ovh.net>
2021-11-11 16:34:59 +0000

    update settings/__init__.py

--- consulting_site/settings/__init__.py
+++ consulting_site/settings/__init__.py
@@ -101,39 +101,40 @@ WSGI_APPLICATION = 'consulting_site.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-                                                                                                                                                                                                         'default': {
-                                                                                                                                                                                                             'ENGINE': 'django.db.backends.sqlite3',
-                                                                                                                                                                                                             'NAME': BASE_DIR / 'db.sqlite3',
-                                                                                                                                                                                                         }
-                                                                                                                                                                                                     }
-                                                                                                                                                                                                    3# Password validation
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+}
+                                                                                                            # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-                                                                                                                                                                                                     AUTH_PASSWORD_VALIDATORS = [
-                                                                                                                                                                                                         {
-                                                                                                                                                                                                         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-                                                                                                                                                                                                         },
-                                                                                                                                                                                                         {
-                                                                                                                                                                                                         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-                                                                                                                                                                                                         },
-                                                                                                                                                                                                         {
-                                                                                                                                                                                                         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-                                                                                                                                                                                                         },
-                                                                                                                                                                                                         {
-                                                                                                                                                                                                         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-                                                                                                                                                                                                         },
-                                                                                                                                                                                                     ]
-                                                                                                                                                                                                     # Internationalization                                                                                                                                                                             
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+AUTH_PASSWORD_VALIDATORS = [
+        {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+]
+
+# Internationalization                                                                                      # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-                                                                                                                                                                                                     TIME_ZONE = 'UTC'
-                                                                                                                                                                                                     USE_I18N = True
-                                                                                                                                                                                                     USE_L10N = True
-                                                                                                                                                                                                     USE_TZ = True
-                                                                                                                                                                                                    
-# Static files (CSS, JavaScript, Images)                                                                                                                                                           
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-                                                                                                                                                                                                    
-STATIC_URL = '/static/'                                                                                                                                                                      
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 INTERNAL_IPS = ['127.0.0.1']
--- consulting_site/settings/production.py
+++ consulting_site/settings/production.py
@@ -1,5 +1,5 @@
 from . import *
 
-SECRET_KEY = 'ùsd544@4§,?65f6s686&793.é65é6à=5=-8+696+755,5'
+SECRET_KEY = 'ùsd544@4§,?65f6s686&79365é6à=5=-8+696+755,5'
 DEBUG = False
 ALLOWED_HOSTS = ['www.adlcconsulting.fr', 'adlcconsulting.fr', '92.222.167.113']
